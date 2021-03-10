8c13ecc19bfb00a1114ed8a18ce36f0fd4ebf18d
Evgenia Kurineva <kurineva85@mail.ru>
2021-03-10 17:30:14 +0300

    feat: add video

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { Box, makeStyles } from '@material-ui/core';
+import posterImg from '../assets/images/main.jpg';
+import { Player, BigPlayButton } from 'video-react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 1170,
+  },
+  info: {
+    display: 'flex',
+  },
+  info_left: {
+    maxWidth: 970,
+    flexGrow:1
+  },
+}));
 
 function Page() {
-   return (
-      <div>
-         Page
-      </div>
-   )
+  const classes = useStyles();
+  return (
+    // <Box bgcolor="primary.light">
+    <Box bgcolor="#edfdff">
+      <Container className={classes.root}>
+        {/* //grid */}
+        <div className={classes.info}>
+          <div className={classes.info_left}>
+            <div>img</div>
+            <div>tekst</div>
+            <div>slider</div>
+          </div>
+          <div className={classes.info_right}>
+            <div>weather</div>
+            <div>date</div>
+            <div>currency</div>
+          </div>
+        </div>
+        {/* //grid */}
+          <Player playsInline poster="/assets/poster.png">
+            <source src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4" />
+            <BigPlayButton position="center" />
+          </Player>
+      </Container>
+      {/*map */}
+    </Box>
+  );
 }
 
-export default Page
+export default Page;
