35d39aebc9352bb3ff60785ec95a7eaf43fb8d92
Ivan work <shukalo.96@gmail.com>
2021-03-11 16:07:55 +0300

    feat: add style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import s from './Header.module.css';
 
 
 function Header() {
   return (
-    <div>  
-      header
+    <div className={s.header}>
+      <div className={s.wrapper}>
+        <div className={s.name}>Travel-app</div>
+        <input type="search" name="search" id={s.search} placeholder='Search'/>
+        <div className={s.language}>
+          <span>Language</span>
+          <select name="lang" id="lang">
+            <option value="1">ru</option>
+            <option value="2">en</option>
+            <option value="3">pl</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
