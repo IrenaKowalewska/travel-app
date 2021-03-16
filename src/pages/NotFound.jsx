fa4014b0b928487df5131b1a8931973088664ad9
Ivan work <shukalo.96@gmail.com>
2021-03-16 14:56:10 +0300

    feat: added page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import backgroundImg from '../assets/images/main.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    alignItems: 'center'
+  },
+  num: {
+    fontSize: 300
+  },
+  content: {
+    fontSize: 50
+  }
+}));
 
 function NotFound({ notFoundMessage }) {
-  
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Not found</h3>
-      <div>404</div>
+    <div className={classes.wrapper}>
+      <div className={classes.num}>404</div>
+  <h3 className={classes.content}>{notFoundMessage}</h3>
     </div>
   );
 }
