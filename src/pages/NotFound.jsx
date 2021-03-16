5aba9bbf60f657cdc9b07f2e25a60ed857fa012a
Evgenia Kurineva <kurineva85@mail.ru>
2021-03-16 14:30:54 +0300

    feat: add Not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Box, Container, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '5rem',
+  },
+  bold: {
+    fontWeight: 600,
+    fontSize: '1rem',
+  },
+}));
 
 function NotFound({ notFoundMessage }) {
-  
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Not found</h3>
-      <div>404</div>
-    </div>
+    <Box bgcolor="primary.light">
+      <Container className={classes.root}>
+        <Typography
+          className={classes.bold}
+          color="textPrimary"
+          component="h2"
+          align="center"
+          display="block"
+        >
+          {notFoundMessage}
+        </Typography>
+      </Container>
+    </Box>
   );
 }
 
